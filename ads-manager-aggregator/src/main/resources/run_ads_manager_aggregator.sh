b5bc47f8cfe684e3e383629e845656ebaa3ebdd4
m.costa <m.costa@coolblue.nl>
2014-12-09 06:58:01 +0100

    fixed aggregator path

--- ads-manager-aggregator/src/main/resources/run_ads_manager_aggregator.sh
+++ ads-manager-aggregator/src/main/resources/run_ads_manager_aggregator.sh
@@ -5,6 +5,6 @@ cd $AGGREGATOR_PATH
 AGGREGATOR_PROPERTIES_FILE=$AGGREGATOR_PATH/src/main/resources/emr.properties
 
 #RUN PIG Script to Aggregate the data and send the aggregations to S3
-java -cp target/ads-manager-aggregator-1.0-SNAPSHOT.jar:target/lib/* ads.manager.aggregator.AdsManagerAggregatorDriver AGGREGATOR_PROPERTIES_FILE
+java -cp target/ads-manager-aggregator-1.0-SNAPSHOT.jar:target/lib/* ads.manager.aggregator.AdsManagerAggregatorDriver $AGGREGATOR_PROPERTIES_FILE
 
 
