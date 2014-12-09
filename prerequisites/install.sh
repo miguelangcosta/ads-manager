49a8c262be51e8771ebb4f7f5fb2de9ff5a9e0db
m.costa <m.costa@coolblue.nl>
2014-12-09 05:32:05 +0100

    fixed no ask install

--- prerequisites/install.sh
+++ prerequisites/install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 #Reinstall Java 7 because Maven probels
-sudo apt-get remove -y openjdk-7-*
-sudo apt-get install -y openjdk-7-jdk
+sudo apt-get remove openjdk-7-*
+sudo apt-get install openjdk-7-jdk
 
 #Set JAVA_HOME
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME/bin
 
 #Install Git
-sudo apt-get install -y git
+sudo apt-get install git
 apt-get update
 
 #Install maven
-sudo apt-get install -y maven
+sudo apt-get install maven
 
 #Clone the repo
 git clone https://github.com/miguelangcosta/ads-manager.git
